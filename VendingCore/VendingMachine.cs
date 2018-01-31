b4d8b1ae3e8ef9597fb83df52ff3a7ed9c15e4c3
J.D. Cain <jd.cain.jr@gmail.com>
2018-01-31 14:43:36 -0500

    basic layout for vending class

--- VendingCore/VendingMachine.cs
+++ VendingCore/VendingMachine.cs
@@ -1,23 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using VendingCore.Model;
 
 namespace VendingCore
 {
     public class VendingMachine
     {
-        
-        private Dictionary<string, decimal> _defaultMoneyTypes => new Dictionary<string, decimal>()
-        {
-            { "Quarter", 0.25m },
-            { "Dime", 0.10m },
-            { "Nickle", 0.05m }
-        };
+        public ReadOnlyCollection<InventoryItem> Shelves => _shelves.AsReadOnly();
+        public ReadOnlyCollection<InventoryItem> Money => _moneyInventory.AsReadOnly();
+        public decimal AvailableFunds { get; protected set; }
 
-        private Dictionary<string, decimal> _moneyDecimals;
-        public VendingMachine(Dictionary<string, decimal> moneyDictionary = null)
+        public VendingMachine(List<InventoryItem> moneyList = null, List<InventoryItem> shelvesList = null)
         {
-            _moneyDecimals = moneyDictionary ?? _defaultMoneyTypes;
+            _moneyInventory = moneyList ?? _defaultMoneyTypes;
+            _shelves = shelvesList ?? _defaultShelves;
         }
+
+        public bool Vend(InventoryItem item)
+        {
+            var result = false;
+            if (item != null 
+                && Shelves.Contains(item) 
+                && (item.Count > 0 
+                    && item.Value >= AvailableFunds))
+            {
+                item.Count--;
+                AvailableFunds -= item.Value;
+                result = true;             
+            }
+            return result;
+        }
+
+        public bool AddMoney(string moneyName)
+        {
+            var money = _moneyInventory.FirstOrDefault(x => x.Name == moneyName);
+            var result = AddMoney(money);
+            return result;
+        }
+        public bool AddMoney(decimal amount)
+        {
+            var money = _moneyInventory.FirstOrDefault(x => x.Value == amount);
+            var result = AddMoney(money);
+            return result;
+        }
+        private bool AddMoney(InventoryItem money)
+        {
+            var result = false;
+            if (money != null)
+            {
+                money.Count++;
+                AvailableFunds += money.Value;
+                result = true;
+            }
+            return result;
+        }
+
+        public bool ReturnMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Private Members
+
+        private readonly List<InventoryItem> _defaultMoneyTypes = new List<InventoryItem>()
+        {
+            new InventoryItem() {Name = "Five", Count = 0, Value = 5m},
+            new InventoryItem() {Name = "Dollar", Count = 0, Value = 1m},
+            new InventoryItem() {Name = "Quarter", Count = 30, Value = 0.25m},
+            new InventoryItem() {Name = "Dime", Count = 25, Value = 0.10m},
+            new InventoryItem() {Name = "Nuckle", Count = 60, Value = 0.05m},
+        };
+        private readonly List<InventoryItem> _defaultShelves = new List<InventoryItem>()
+        {
+            new InventoryItem() {Name = "Chips", Count = 10, Value = 0.75m},
+            new InventoryItem() {Name = "Pop", Count = 10, Value = 1.25m},
+            new InventoryItem() {Name = "Gum", Count = 10, Value = 0.50m},
+            new InventoryItem() {Name = "Pretzels", Count = 10, Value = 0.75m},
+        };
+        private readonly List<InventoryItem> _moneyInventory;
+        private readonly List<InventoryItem> _shelves;
+
+        #endregion
     }
 }
