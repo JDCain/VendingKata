b375edddd019e278cf515a4470737f380d69d029
J.D. Cain <jd.cain.jr@gmail.com>
2018-01-31 14:43:20 -0500

    added inventory item type

---
+++ VendingCore/Model/InventoryItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace VendingCore.Model
+{
+    public class InventoryItem
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public decimal Value { get; set; }
+    }
+}
