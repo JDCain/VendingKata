9fe4e7012e8d6d431ccee96bc32ac76af0104355
J.D. Cain <jd.cain.jr@gmail.com>
2018-01-31 11:19:15 -0500

    default class & constructor

--- VendingCore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VendingCore
-{
-    public class Class1
-    {
-    }
-}
---
+++ VendingCore/VendingMachine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace VendingCore
+{
+    public class VendingMachine
+    {
+        
+        private Dictionary<string, decimal> _defaultMoneyTypes => new Dictionary<string, decimal>()
+        {
+            { "Quarter", 0.25m },
+            { "Dime", 0.10m },
+            { "Nickle", 0.05m }
+        };
+
+        private Dictionary<string, decimal> _moneyDecimals;
+        public VendingMachine(Dictionary<string, decimal> moneyDictionary = null)
+        {
+            _moneyDecimals = moneyDictionary ?? _defaultMoneyTypes;
+        }
+    }
+}
