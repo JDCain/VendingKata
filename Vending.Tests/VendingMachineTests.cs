5c637d6e3f966a553e2048179b036a32c31282ae
J.D. Cain <jd.cain.jr@gmail.com>
2018-01-31 21:13:57 -0500

    Unit test for adding decimals

--- Vending.Tests/VendingMachineTests.cs
+++ Vending.Tests/VendingMachineTests.cs
@@ -14,13 +14,31 @@ namespace Vending.Tests
             AddMoneyStringAssert("Quarter", 0.25m);
             AddMoneyStringAssert("Dime", 0.10m);
             AddMoneyStringAssert("Nickel", 0.05m);
-
+            var machine = new VendingMachine();
+            Assert.IsFalse(machine.AddMoney("Hamburger"));
+        }
+        [TestMethod]
+        public void AddMoneyDefaultValues()
+        {
+            AddMoneyDecimalAssert(5m);
+            AddMoneyDecimalAssert(1m);
+            AddMoneyDecimalAssert(0.25m);
+            AddMoneyDecimalAssert(0.10m);
+            AddMoneyDecimalAssert(0.05m);
+            var machine = new VendingMachine();
+            Assert.IsFalse(machine.AddMoney(0.36m));
         }
         private static void AddMoneyStringAssert(string amount, decimal expectedValue)
         {
             var machine = new VendingMachine();
             machine.AddMoney(amount);
             Assert.IsTrue(machine.AvailableFunds == expectedValue);
+        }
+        private static void AddMoneyDecimalAssert(decimal amount)
+        {
+            var machine = new VendingMachine();
+            machine.AddMoney(amount);
+            Assert.IsTrue(machine.AvailableFunds == amount);
         }
     }
 }
